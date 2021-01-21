cc522fef314c98cf78b08811d9d3a8c10452309b
fernando-desktop <fjprudenciop@gmail.com>
2021-01-20 21:11:04 -0500

    minor changes

--- scripts/remove_trend.R
+++ scripts/remove_trend.R
@@ -49,11 +49,11 @@ lines(ts_d2d12, type = "l", col = "blue")
 #---------------------------#
 install.packages("fpp")
 library(fpp)
+library(forecast)
 data(ausbeer)
 timeserie_beer <- tail(head(ausbeer, 17*4+2),17*4-4)
 plot(as.ts(timeserie_beer))
 #--------#
-library(forecast)
 trend_beer <- ma(timeserie_beer, order = 4, centre = T)
 plot(as.ts(timeserie_beer))
 lines(trend_beer)
@@ -127,3 +127,71 @@ plot(as.ts(decompose_air$seasonal))
 plot(as.ts(decompose_air$trend))
 plot(as.ts(decompose_air$random))
 plot(decompose_air)
+
+
+# HOW TO CALCULATE ANOMALIES ON TIME SERIES WITH OUTLIERS
+set.seed(4)
+data <- read.csv("data/tables/webTraffic.csv", sep = ",", header = T)
+head(data)
+tail(data)
+days <- as.numeric(data$Visite)
+for (i in 1:45) {
+  pos <- floor(runif(1, 1, 50))
+  days[i * 15 + pos] <- days[i * 15 + pos]^1.2
+}
+days[510 + pos] <- 0
+plot(as.ts(days))
+
+#-----#
+# DESCOMPOSICIÓN DE LA SERIE DE TIEMPO
+# install required library
+install.packages("FBN")
+library(FBN)
+
+decomposed_days <- decompose(ts(days, frequency = 7), "multiplicative")
+plot(decomposed_days)
+
+# USO DE LA DISTRIBUCIÓN NORMAL PARA ENCONTRAR EL MÍNIMO Y EL MÁXIMO
+random <- decomposed_days$random
+min <- mean(random, na.rm = T) - 4 * sd(random, na.rm = T)
+max <- mean(random, na.rm = T) + 4 * sd(random, na.rm = T)
+
+plot(as.ts(as.vector(random)), ylim = c(-0.5, 2.5))
+abline(h = max, col = "#e15f3f", lwd = 2)
+abline(h = min, col = "#e15f3f", lwd = 2)
+
+#install library
+install.packages("forecast")
+library(forecast)
+library(stats)
+
+trend <- runmed(days, 7)
+plot(as.ts(trend))
+
+detrend <- days / as.vector(trend)
+m <- t(matrix(data = detrend, nrow = 7))
+seasonal <- colMeans(m, na.rm = T)
+random <- days / (trend * seasonal)
+rm_random <- runmed(random[!is.na(random)], 3)
+
+min <- mean(rm_random, na.rm = T) - 4 * sd(rm_random, na.rm = T)
+max <- mean(rm_random, na.rm = T) + 4 * sd(rm_random, na.rm = T)
+plot(as.ts(random))
+abline(h = max, col = "#e15f3f", lwd = 2)
+abline(h = min, col = "#e15f3f", lwd = 2)
+#--------------------------#
+high <- (trend * seasonal) * max
+low <- (trend * seasonal) * min
+plot(as.ts(days), ylim = c(0, 200))
+lines(as.ts(high), col = "blue")
+lines(as.ts(low), col = "red")
+
+x <- c(seq(1, length(high)), seq(length(high), 1))
+y <- c(high, rev(low))
+length(x)
+length(y)
+polygon(x, y, density = 50, col = "skyblue")
+
+
+
+
